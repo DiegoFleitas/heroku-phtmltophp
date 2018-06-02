572bdcd389d7da6b79ad4833440eeed0478e4185
dfleitas <diego.fleitas68@gmail.com>
2018-06-01 21:28:51 -0300

    added database access

--- web/index.php
+++ web/index.php
@@ -27,5 +27,41 @@ $app->get('/cowsay', function() use($app) {
     return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
 });
 
+$app->get('/', function() use($app) {
+    $app['monolog']->addDebug('logging output.');
+    return str_repeat('Hello', getenv('TIMES'));
+});
+
+//PDO connection
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+    array(
+        'pdo.server' => array(
+            'driver'   => 'pgsql',
+            'user' => $dbopts["user"],
+            'password' => $dbopts["pass"],
+            'host' => $dbopts["host"],
+            'port' => $dbopts["port"],
+            'dbname' => ltrim($dbopts["path"],'/')
+        )
+    )
+);
+
+//query
+$app->get('/db/', function() use($app) {
+    $st = $app['pdo']->prepare('SELECT name FROM test_table');
+    $st->execute();
+
+    $names = array();
+    while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+        $app['monolog']->addDebug('Row ' . $row['name']);
+        $names[] = $row;
+    }
+
+    return $app['twig']->render('database.twig', array(
+        'names' => $names
+    ));
+});
+
 
 $app->run();
